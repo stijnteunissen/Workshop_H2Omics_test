a73dec3ec3839f63ef838c5d224b18a9226eaa8c
stijnteunissen <stijnteunissen@ziggo.nl>
2025-04-08 13:39:30 +0200

    update

--- H2Omics_analysis/install_packages.R
+++ H2Omics_analysis/install_packages.R
@@ -1,6 +1,6 @@
 install_packages <- function() {
   # Define the custom library folder on your Google Drive for phyloseq
-  drive_lib <- "/content/Workshop_H2Omics_test/H2Omics_analysis/Rlibs"
+  drive_lib <- "/content/drive/MyDrive/Rlibs"  # "/content/Workshop_H2Omics_test/H2Omics_analysis/Rlibs"
   if (!dir.exists(drive_lib)) {
     dir.create(drive_lib, recursive = TRUE)
     message("Custom library folder created at: ", drive_lib)
@@ -199,6 +199,11 @@ install_packages <- function() {
     devtools::install_github("jbisanz/qiime2R", quiet = TRUE)
   }
   suppressMessages(library(qiime2R))
+
+  if (!requireNamespace("tidyverse", quietly = TRUE)) {
+    devtools::install_github("tidyverse", quiet = TRUE)
+  }
+  suppressMessages(library(tidyverse))
 
   if (!requireNamespace("RasperGade", quietly = TRUE)) {
     devtools::install_github(repo = "wu-lab-uva/RasperGade", quiet = TRUE)
