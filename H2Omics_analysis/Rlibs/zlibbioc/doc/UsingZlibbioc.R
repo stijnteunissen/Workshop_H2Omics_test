4f98c6c9bf4b31754338a0c4e1ede0a6730b7141
stijnteunissen <stijnteunissen@ziggo.nl>
2025-04-09 15:36:16 +0200

    update

--- H2Omics_analysis/Rlibs/zlibbioc/doc/UsingZlibbioc.R
+++
@@ -1,7 +0,0 @@
-## ----eval=FALSE---------------------------------------------------------------
-#  R CMD INSTALL --configure-args="--with-libzbioc" zlibioc_<...>.tar.gz
-
-## ----eval=FALSE---------------------------------------------------------------
-#  install.packages("zlibbioc_<...>.tar.gz",
-#                     configure.args="--with-libzbioc")
-
