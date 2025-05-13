32531590ffa73582df8d3bc28602e266decb83e2
stijnteunissen <stijnteunissen@ziggo.nl>
2025-05-13 15:14:57 +0200

    update

--- H2Omics_analysis/copy_number_prediction.R
+++ H2Omics_analysis/copy_number_prediction.R
@@ -3,8 +3,14 @@ copy_number_prediction = function() {
 # Set the qiime2 output directory (using base_path and projects)
 qiime_output_dir <- file.path(base_path, projects, "qiime2_output")
 
+qza_file = list.files(qiime_output_dir, pattern = "representative_sequences\\.qza$", full.names = TRUE)
+temp_dir = tempfile("rep_seq_")
+dir.create(temp_dir)
+utils::unzip(qza_file, exdir = temp_dir)
+
 # Find the FASTA file that ends with "dna-sequence.FASTA"
-fasta_file <- list.files(qiime_output_dir, pattern = "dna-sequences\\.fasta$", full.names = TRUE)[1]
+fasta_file = list.files(file.path(temp_dir, "data"), pattern = "dna-sequences\\.fasta$", full.names = TRUE)
+#fasta_file <- list.files(qiime_output_dir, pattern = "dna-sequences\\.fasta$", full.names = TRUE)[1]
 if (is.na(fasta_file)) stop("No FASTA file found in ", qiime_output_dir)
 
 # Run the 16S GCN prediction using the FASTA file
