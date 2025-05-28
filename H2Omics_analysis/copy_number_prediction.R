e97432c6dce7c9bec6a553feb80e688e228fb720
stijnteunissen <stijnteunissen@ziggo.nl>
2025-05-28 10:15:15 +0200

    update

--- H2Omics_analysis/copy_number_prediction.R
+++ H2Omics_analysis/copy_number_prediction.R
@@ -1,5 +1,7 @@
 copy_number_prediction = function() {
 
+Sys.setenv(PATH = paste("/content/Workshop_H2Omics_test/H2Omics_analysis/tools", Sys.getenv("PATH"), sep = ":"))
+
 # Set the qiime2 output directory (using base_path and projects)
 qiime_output_dir <- file.path(base_path, projects, "qiime2_output")
 
